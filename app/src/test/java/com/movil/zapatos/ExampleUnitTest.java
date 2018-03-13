dec16a923b690fd3e96e3282c8c4fe8f1265d361
AndresCampo <andrescampoe@gmail.com>
2018-03-12 20:45:16 -0500

    First release tested successful

--- app/src/test/java/com/movil/zapatos/ExampleUnitTest.java
+++ app/src/test/java/com/movil/zapatos/ExampleUnitTest.java
@@ -14,4 +14,24 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    @Test
+    public void precioZapatillaMujerNike(){
+        double precio = Metodos.Calcular(1,0,1,1);
+        assertEquals(100000,precio,0);
+    }
+    @Test
+    public void precioClasicoMujerPuma(){
+        double precio = Metodos.Calcular(1,2,0,1);
+        assertEquals(120000,precio,0);
+    }
+    @Test
+    public void precioZapatillasHombreAdidas(){
+        double precio = Metodos.Calcular(0,1,1,1);
+        assertEquals(140000,precio,0);
+    }
+    @Test
+    public void precioClasicoHombreNike(){
+        double precio = Metodos.Calcular(0,0,0,1);
+        assertEquals(50000,precio,0);
+    }
 }
